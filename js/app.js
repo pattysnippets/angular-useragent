2d7eb2449c524c4669264dd945ee89e0784f2e5a
Joyce Cam <joycep.cam@gmail.com>
2014-12-30 17:27:22 +1000

    useragent files

---
+++ js/app.js
@@ -0,0 +1,160 @@
+var app = angular.module('useragent', []);
+
+app.controller('MainCtrl', function($scope, useragent) {
+  $scope.useragent = useragent.get();
+  $scope.appCodeName = useragent.appCodeName();
+  $scope.browserName = useragent.browserName();
+  $scope.platform = useragent.platform();
+  $scope.appVersion = useragent.appVersion();
+  $scope.version = useragent.version();
+  $scope.OS = useragent.OS();
+  $scope.device = useragent.device();
+  $scope.cookie = useragent.cookie();
+  $scope.lang = useragent.lang();
+  $scope.online = useragent.online(); 
+});
+
+	var n = navigator,
+		Agent = n.userAgent,
+		CodeName = n.appCodeName,
+		appVersion = n.appVersion,
+		browserName = n.appName,
+		platform = n.platform,
+		cookie = n.cookieEnabled,
+		language = n.language,
+		online = n.onLine,
+		fullVersion = '' + parseFloat(n.appVersion),
+		BrMajorVersion = parseInt(n.appVersion, 10),
+		OffsetName, objOffsetVersion, ix,
+		OSname = 'unknownUS';
+
+	// In Chrome
+	if ((objOffsetVersion = Agent.indexOf("Chrome")) != -1) {
+		browserName = "Chrome";
+		fullVersion = Agent.substring(objOffsetVersion + 7);
+	}
+	// In Microsoft internet explorer
+	else if ((objOffsetVersion = Agent.indexOf("MSIE")) != -1) {
+		browserName = "Microsoft Internet Explorer";
+		fullVersion = Agent.substring(objOffsetVersion + 5);
+	}
+	// In Firefox
+	else if ((objOffsetVersion = Agent.indexOf("Firefox")) != -1) {
+		browserName = "Firefox";
+	}
+	// In Safari
+	else if ((objOffsetVersion = Agent.indexOf("Safari")) != -1) {
+		browserName = "Safari";
+		fullVersion = Agent.substring(objOffsetVersion + 7);
+		if ((objOffsetVersion = Agent.indexOf("Version")) != -1) fullVersion = Agent.substring(objOffsetVersion + 8);
+	}
+	// For other browser "name/version" is at the end of userAgent
+	else if ((OffsetName = Agent.lastIndexOf(' ') + 1) < (objOffsetVersion = Agent.lastIndexOf('/'))) {
+		browserName = Agent.substring(OffsetName, objOffsetVersion);
+		fullVersion = Agent.substring(objOffsetVersion + 1);
+		if (browserName.toLowerCase() == browserName.toUpperCase()) {
+			browserName = navigator.appName;
+		}
+	}
+	// trimming the fullVersion string at semicolon/space if present
+	if ((ix = fullVersion.indexOf(";")) != -1) fullVersion = fullVersion.substring(0, ix);
+	if ((ix = fullVersion.indexOf(" ")) != -1) fullVersion = fullVersion.substring(0, ix);
+	BrMajorVersion = parseInt('' + fullVersion, 10);
+	if (isNaN(BrMajorVersion)) {
+		fullVersion = '' + parseFloat(navigator.appVersion);
+		BrMajorVersion = parseInt(navigator.appVersion, 10);
+	}
+
+	//OS
+	if (appVersion.indexOf("Win") != -1) {
+		OSname = 'windowsOS';
+	}
+	if (appVersion.indexOf("Mac") != -1) {
+		OSname = 'macOS';
+	}
+	if (appVersion.indexOf("X11") != -1) {
+		OSname = 'unixOS';
+	}
+	if (appVersion.indexOf("Linux") != -1) {
+		OSname = 'linuxOS';
+	}
+
+	//Device
+	var mobile = {
+		Android: function () {
+			return Agent.match(/Android/i);
+		},
+		BlackBerry: function () {
+			return Agent.match(/BlackBerry/i);
+		},
+		iOS: function () {
+			return Agent.match(/iPhone|iPad|iPod/i);
+		},
+		Opera: function () {
+			return Agent.match(/Opera Mini/i);
+		},
+		Windows: function () {
+			return Agent.match(/IEMobile/i);
+		},
+		any: function () {
+			return (mobile.Android() || mobile.BlackBerry() || mobile.iOS() || mobile.Opera() || mobile.Windows());
+		}
+	}; 
+
+
+app.factory('useragent', function () {
+
+		return {
+			get: function () {
+				return Agent;
+			},
+
+			appCodeName: function () {
+				return CodeName;
+			},
+
+			appName: function () {
+				return browserName;
+			},
+
+			browserName: function () {
+				return browserName;
+			},
+
+			appVersion: function () {
+				return appVersion;
+			},
+
+			platform: function () {
+				return platform;
+			},
+
+			version: function () {
+				return BrMajorVersion;
+			},
+
+			OS: function () {
+				return OSname;
+			},
+
+			cookie: function () {
+				return cookie;
+			},
+
+			lang: function () {
+				return language;
+			},
+
+			online: function () {
+				return online;
+			},
+
+			device: function () {
+			  var d = 'null';
+			  if (mobile.any())
+			    d = mobile.any();
+				return d;
+			}
+		}
+
+	})
